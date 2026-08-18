d4c0e9fe9c3e732c8f21fae7aa435d536760b2ff
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-08-18 16:41:52 +0200

    fix: update import paths in index.ts for consistency

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export { default as ProductCard } from "../product/ProductCard";
-export { default as CategoryCard } from "../product/CategoryCard";
-export { default as BannerCarousel } from "../layout/BannerCarousel";
-export { default as TrustBadges } from "../layout/TrustBadges";
-export { default as Header } from "../layout/Header";
-export { default as WhatsAppButton } from "../layout/WhatsAppButton";
-export { EmptyState, LoadingSpinner, ErrorState } from "../ui/States";
+export { default as ProductCard } from "./product/ProductCard";
+export { default as CategoryCard } from "./product/CategoryCard";
+export { default as BannerCarousel } from "./layout/BannerCarousel";
+export { default as TrustBadges } from "./layout/TrustBadges";
+export { default as Header } from "./layout/Header";
+export { default as WhatsAppButton } from "./layout/WhatsAppButton";
+export { EmptyState, LoadingSpinner, ErrorState } from "./ui/States";
